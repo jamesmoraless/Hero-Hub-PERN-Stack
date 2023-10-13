64306543e2da3ff324c133d64309162ba088ca16
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2023-10-13 00:34:20 -0400

    first backend commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,69 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
-//app.use(express.json()); //middleware that makes the request into a json object
-//app.use(express.static("public")); //this middleware gets the static files in public folder and executes this before anything below
-//app.use(cors({
-//    origin: 'http://localhost:3000'//must update when the front-end is deployed
-//}));
+const PORT = 5000;
+const DATA_PATH = path.join(__dirname, 'superhero_info.json');
+const POWERS_PATH = path.join(__dirname, 'superhero_powers.json');
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// Use body-parser middleware to handle JSON requests
+app.use(bodyParser.json());
+
+// Serving static files from the client directory
+app.use(express.static(path.join(__dirname, '../client')));
+
+
+app.get('/api/superhero/:id', (req, res) => {//gets all  the fields based on id BUT I will make it by name
+    fs.readFile('superhero_info.json', 'utf8', (err, data) => {
+        if (err) return res.status(500).json({ error: 'Failed to read data' });
+        
+        const superheroes = JSON.parse(data);
+        const superhero = superheroes.find(hero => hero.id.toString() === req.params.id);
+        
+        if (!superhero) return res.status(404).json({ error: 'Superhero not found' });
+
+        res.json(superhero);
+    });
 });
 
-app.listen(3000, () => console.log(`its alive on http://localhost:${PORT}`)); //runs the API on a server defined by the port (port location, function)
+app.get('/api/powers/:id', (req, res) => {
+    fs.readFile('superhero_info.json', 'utf8', (err, heroData) => {
+        if (err) return res.status(500).json({ error: 'Failed to read superhero data' });
+
+        const superheroes = JSON.parse(heroData);
+        const superhero = superheroes.find(hero => hero.id.toString() === req.params.id);
+
+        if (!superhero) return res.status(404).json({ error: 'Superhero not found' });
+
+        fs.readFile('superhero_powers.json', 'utf8', (err, powersData) => {
+            if (err) return res.status(500).json({ error: 'Failed to read powers data' });
+
+            const powersList = JSON.parse(powersData);
+            const heroPowers = powersList.find(hero => hero.hero_names === superhero.name);
+
+            if (!heroPowers) return res.status(404).json({ error: 'Superhero powers not found' });
+
+            const truePowers = Object.keys(heroPowers).filter(key => heroPowers[key] === "True");
+            
+            res.json({ powers: truePowers });
+        });
+    });
+});
+
+app.get('/api/publishers', (req, res) => {
+    fs.readFile('superhero_info.json', 'utf8', (err, data) => {
+        if (err) return res.status(500).json({ error: 'Failed to read data' });
+
+        const superheroes = JSON.parse(data);
+        const publishers = [...new Set(superheroes.map(hero => hero.Publisher))];
+
+        res.json({ publishers });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
