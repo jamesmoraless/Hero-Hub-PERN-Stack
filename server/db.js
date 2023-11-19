d6ea79a586fcf8bb15c5721763ef126887e0c4cb
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2023-11-19 01:01:10 -0500

    changed server comments

--- server/db.js
+++ server/db.js
@@ -1,13 +1,7 @@
 const Pool = require('pg').Pool;
-//const dotenv = require('dotenv');
-
-//dotenv.config();
 
 const pool = new Pool({ connectionString: 'postgres://postgres:Williamandoraul1$@localhost:5433/webtech' });
 
 module.exports = pool;
 
-//process.on('exit', () => {
-//    pool.end();
-//  });
   
